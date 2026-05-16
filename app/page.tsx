7aea3855f218d4c306fc139e662b2d00403c6ff3
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-05-16 19:26:26 +0200

    develop - Fix link href

--- app/page.tsx
+++ app/page.tsx
@@ -148,7 +148,7 @@ export default function HomePage() {
           asChild
           className="px-10 py-5 text-lg rounded-2xl bg-indigo-600 hover:bg-indigo-700"
         >
-          <Link href="/create">Créer mon dossier LocProof</Link>
+          <Link href="/create/me">Créer mon dossier LocProof</Link>
         </Button>
       </section>
     </AppLayout>
