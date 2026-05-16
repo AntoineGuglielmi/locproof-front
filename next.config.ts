11888f9aa757bc64cb8e45a89f2ac7814ac8e557
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-05-16 18:42:33 +0200

    006 - Update next config and update .env.exemple

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+const {
+  REMOTE_PATTERN_HOSTNAME,
+  REMOTE_PATTERN_PORT,
+  REMOTE_PATTERN_PROTOCOL,
+} = process.env
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: REMOTE_PATTERN_PROTOCOL as 'http' | 'https',
+        hostname: REMOTE_PATTERN_HOSTNAME!,
+        ...(REMOTE_PATTERN_PORT ? { port: REMOTE_PATTERN_PORT } : {}),
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
