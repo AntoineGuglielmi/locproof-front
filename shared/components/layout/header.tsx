8646538c30fe976b6dff5e7257ddce5cc9062df0
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-05-16 19:20:08 +0200

    006 - Try unoptimize logo

--- shared/components/layout/header.tsx
+++ shared/components/layout/header.tsx
@@ -14,6 +14,7 @@ export default function Header() {
           width={50}
           height={50}
           className="size-8"
+          unoptimized
         />{' '}
         LocProof
       </Link>
