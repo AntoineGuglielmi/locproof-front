86bf0ba4153a47af1cdeb045e423bf593433074e
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-05-19 22:05:45 +0200

    010 - Implement the you-got-a-request email template

--- lib/email.ts
+++ lib/email.ts
@@ -1,4 +1,5 @@
 import EmailValidation from '@/features/Email/components/templates/email-validation'
+import YouGotARequest from '@/features/Email/components/templates/you-got-a-request'
 import { Rental, Tenant } from '@/types/strapi-types'
 import { Resend } from 'resend'
 
@@ -28,24 +29,13 @@ export const sendEmailToLandlord = async ({
   landlordEmail: Rental['landlordEmail']
   rentalToken: Rental['rentalToken']
 }) => {
-  const url = `${process.env.NEXT_PUBLIC_APP_URL}/api/check-rental?rentalToken=${rentalToken}`
+  const href = `${process.env.NEXT_PUBLIC_APP_URL}/api/check-rental?rentalToken=${rentalToken}`
   await resend.emails.send({
     from: `LocProof <${process.env.RESEND_EMAIL_FROM}>`,
     to: landlordEmail!,
     subject:
       'Vous avez reçu une demande de recommandation de la part de votre ancien locataire',
-    html: `
-      <p>Bonjour,</p>
-      <p>Votre ancien locataire vous a demandé une recommandation via LocProof, la plateforme de recommandation de location.</p>
-      <p>Pour consulter cette demande et rédiger votre recommandation, cliquez simplement sur le lien ci-dessous :</p>
-      <p><a href="${url}">👉 Rédiger ma recommandation</a></p>
-      <p>Ce lien est valable pendant 7 jours.</p>
-      <p>Si vous ne souhaitez pas rédiger de recommandation, vous pouvez ignorer cet email.</p>
-      <p></p>
-      <p>—</p>
-      <p></p>
-      <p>LocProof</p>
-    `,
+    react: YouGotARequest({ href }),
   })
 }
 
