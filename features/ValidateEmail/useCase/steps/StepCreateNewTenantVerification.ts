f13fc0640bb6d2d59a0f1454c3dea36254996df9
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-08-04 00:57:55 +0200

    016 - Enforce step context requirements in ValidateEmail useCase

--- features/ValidateEmail/useCase/steps/StepCreateNewTenantVerification.ts
+++ features/ValidateEmail/useCase/steps/StepCreateNewTenantVerification.ts
@@ -1,10 +1,10 @@
 import { Step } from '@/shared/core/useCase/Step'
-import { TypeContextValidateEmail } from '../../types/TypeContextValidateEmail'
 import { tenantVerificationRepository } from '@/repositories/tenant-verification.repository'
+import { TypeContextWithEmail } from '../../types/TypesSteps'
 
-export class StepCreateNewTenantVerification extends Step<TypeContextValidateEmail> {
-  async execute(context: TypeContextValidateEmail): Promise<void> {
-    const email = context.email!
+export class StepCreateNewTenantVerification extends Step<TypeContextWithEmail> {
+  async execute(context: TypeContextWithEmail): Promise<void> {
+    const { email } = context
 
     const tenantVerificationToken = crypto.randomUUID()
 
