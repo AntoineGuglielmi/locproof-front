8b9a6055bf3ad1244dadd4d8b01b3d9c2b3b119b
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-08-07 01:30:38 +0200

    020 - Refacto page error handling

--- shared/entities/EntityTenantVerification.ts
+++ shared/entities/EntityTenantVerification.ts
@@ -6,12 +6,12 @@ export class EntityTenantVerification {
   private _tenantVerificationToken: TenantVerification['tenantVerificationToken']
   private _state: TenantVerification['state']
 
-  constructor({
-    email,
-    expiresAt,
-    tenantVerificationToken,
-    state,
-  }: TenantVerification) {
+  constructor(tenantVerification: TenantVerification | null) {
+    if (tenantVerification === null) {
+      return
+    }
+    const { email, expiresAt, tenantVerificationToken, state } =
+      tenantVerification
     this._email = email
     this._expiresAt = expiresAt
     this._tenantVerificationToken = tenantVerificationToken
