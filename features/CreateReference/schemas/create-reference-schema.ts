1184a99ff05b357b3db4378b40c096a056625b0e
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-08-09 15:00:49 +0200

    021 - Add tests for schema

--- features/CreateReference/schemas/create-reference-schema.ts
+++ features/CreateReference/schemas/create-reference-schema.ts
@@ -1,11 +1,8 @@
 import z from 'zod'
 
-export const referenceAnswerSchema = z
-  .enum(['yes', 'no', 'skip'])
-  .optional()
-  .refine((value) => value !== undefined, {
-    message: 'Veuillez sélectionner une réponse',
-  })
+export const referenceAnswerSchema = z.enum(['yes', 'no', 'skip'], {
+  message: 'Veuillez sélectionner une réponse',
+})
 
 export const createReferenceSchema = z.object({
   paidOnTime: referenceAnswerSchema,
