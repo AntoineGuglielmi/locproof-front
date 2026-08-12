d6913e250c832eeb6a78bae67583fcbdaea8d0e4
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-08-12 15:05:21 +0200

    025 - Create custom button component

---
+++ shared/components/form/button.tsx
@@ -0,0 +1,72 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import { cn } from '@/shared/lib/className'
+import { Button as ShadcnButton } from '../shadcn/ui/button'
+
+const customButtonVariants = cva('Button rounded-xl', {
+  variants: {
+    variant: {
+      default: 'bg-indigo-600 hover:bg-indigo-700',
+      outline: '',
+      outlineDarker: 'hover:bg-gray-300',
+      dark: 'bg-primary',
+      link: '',
+    },
+
+    size: {
+      default: 'px-8 py-6 text-base',
+      sm: 'rounded-lg',
+    },
+  },
+
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+})
+
+const shadcnVariants = {
+  default: 'default',
+  outline: 'outline',
+  outlineDarker: 'outline',
+  dark: 'default',
+  link: 'link',
+} as const
+
+const shadcnSizes = {
+  default: 'default',
+  sm: 'sm',
+} as const
+
+type ButtonProps = Omit<
+  React.ComponentProps<typeof ShadcnButton>,
+  'variant' | 'size'
+> &
+  VariantProps<typeof customButtonVariants>
+
+export default function Button({
+  className,
+  variant,
+  size,
+  ...props
+}: ButtonProps) {
+  const customVariant = variant ?? 'default'
+  const customSize = size ?? 'default'
+
+  const shadcnVariant = shadcnVariants[customVariant]
+  const shadcnSize = shadcnSizes[customSize]
+
+  return (
+    <ShadcnButton
+      {...props}
+      variant={shadcnVariant}
+      size={shadcnSize}
+      className={cn(
+        customButtonVariants({
+          variant: customVariant,
+          size: customSize,
+        }),
+        className,
+      )}
+    />
+  )
+}
