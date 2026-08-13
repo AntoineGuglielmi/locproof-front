cf747db64588f1e4ac9f3fa00729257fad9e7fb2
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-08-14 01:27:36 +0200

    028 - Update AppLayout paddings

--- shared/components/layout/app-layout.tsx
+++ shared/components/layout/app-layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <div className="flex flex-col min-h-svh">
       <Header />
       {process.env.DEBUG === '1' && <Debug />}
-      <main className="py-8 flex-1">
+      <main className="pt-16 md:pt-24 pb-20 md:pb-34 flex-1">
         <Grid>{children}</Grid>
       </main>
       <Footer />
