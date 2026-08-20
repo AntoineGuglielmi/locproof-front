e6731a3382a345f80d3d19a78afd134ed5c4d9d0
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-08-21 01:01:32 +0200

    036 - Create an EmailButton component and use it to harmonize emails button with he product buttons

--- features/RetrieveProfile/components/retrieve-profile-email.tsx
+++ features/RetrieveProfile/components/retrieve-profile-email.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
+import EmailButton from '@/features/Emails/components/EmailButton'
 import {
   Html,
   Heading,
   Tailwind,
   pixelBasedPreset,
   Text,
-  Link,
   Hr,
   Head,
   Body,
@@ -36,26 +36,23 @@ export default function RetrieveProfileEmail({ href }: RetrieveProfileProps) {
         <Body className="mx-auto my-auto bg-white px-2 font-sans">
           <Container className="mx-auto my-10 max-w-116.25 rounded-2xl border border-[#eaeaea] border-solid p-5">
             <Preview>
-              Cliquez sur le lien fourni pour accéder à votre profile Locproof
+              Cliquez sur le lien fourni pour accéder à votre profile LocProof
             </Preview>
             <Heading as="h2">Bonjour</Heading>
 
-            <Text>Vous avez demandé à accéder à votre profile Locproof.</Text>
+            <Text>Voous avez demandé à retrouver votre profil LocProof.</Text>
             <Text>
-              Pour voir votre profile, cliquez simplement sur le lien ci-dessous
-              :
+              Pour y accéder, cliquez simplement sur le bouton ci-dessous :
             </Text>
             <Text>
-              <Link
-                href={href}
-                className="inline-flex gap-4 items-center rounded-full py-2 px-4 text-base text-white bg-indigo-600"
-              >
-                Voir mon profil
-              </Link>
+              <EmailButton {...{ href }}>Voir mon profil</EmailButton>
             </Text>
             <Text>
-              Si vous pensez qu'il y a une erreur ou si vous avez des questions,
-              n'hésitez pas à contacter notre support.
+              Si vous n’êtes pas à l’origine de cette demande, vous pouvez
+              ignorer cet email.
+            </Text>
+            <Text>
+              Pour toute question, n’hésitez pas à contacter notre équipe.{' '}
             </Text>
             <Hr />
             <Text className="font-bold">
