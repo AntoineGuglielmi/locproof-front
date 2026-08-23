36421896001c438da4fa108560d99cbeb8e34ede
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-08-24 00:06:30 +0200

    037 - Fix check-rental use case

--- features/API/CheckRental/useCase/UseCaseCheckRental.ts
+++ features/API/CheckRental/useCase/UseCaseCheckRental.ts
@@ -2,7 +2,14 @@ import { UseCase } from '@/shared/core/useCase/UseCase'
 import { StepGetRental } from './steps/StepGetRental'
 import { StepValidateRental } from './steps/StepValidateRental'
 import { StepCheckReference } from './steps/StepCheckReference'
+import { StepCheckRentalToken } from './steps/StepCheckRentalToken'
+import { TypeContextCheckRental } from '../types/TypeContextCheckRental'
 
-export class UseCaseCheckRental extends UseCase<object> {
-  steps = [StepGetRental, StepValidateRental, StepCheckReference]
+export class UseCaseCheckRental extends UseCase<TypeContextCheckRental> {
+  steps = [
+    StepCheckRentalToken,
+    StepGetRental,
+    StepValidateRental,
+    StepCheckReference,
+  ]
 }
